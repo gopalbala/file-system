39fe403419f09f74799b39e9a95cf55e5a388165
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-10-20 00:38:51 +0530

    added get children, find directory under this and sort

--- src/main/java/com/gb/filesystem/Directory.java
+++ src/main/java/com/gb/filesystem/Directory.java
@@ -8,11 +8,34 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-public class Directory extends FileSystemElement{
+public class Directory extends FileSystemElement {
     private Directory parent;
     private List<FileSystemElement> children = new ArrayList<>();
-    public Directory(Directory parent,String name,String owner,int size){
-        super(parent,name,owner,size);
+
+    public Directory(Directory parent, String name, String owner, int size) {
+        super(parent, name, owner, size);
     }
 
+    public void addChild(FileSystemElement child) {
+        super.setFile(false);
+        children.add(child);
+    }
+
+    public int getSize() {
+        int total = 0;
+        for (FileSystemElement fElement : this.getChildren()) {
+            total = total + fElement.getSize();
+        }
+        return total;
+    }
+
+    public Directory getDirectory(String dirName) {
+        Directory directory = null;
+        for (FileSystemElement fs : this.getChildren()) {
+            if (fs.getName().contentEquals(dirName) && fs instanceof Directory) {
+                directory = (Directory) fs;
+            }
+        }
+        return directory;
+    }
 }
