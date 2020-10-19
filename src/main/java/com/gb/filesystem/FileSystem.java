0eaf1a0872f0445315cbe3e469f71b3ba964cf8d
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-10-20 00:38:05 +0530

    added file system

---
+++ src/main/java/com/gb/filesystem/FileSystem.java
@@ -0,0 +1,39 @@
+package com.gb.filesystem;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@Getter
+@Setter
+public class FileSystem {
+    private Directory directory;
+    private Directory current;
+    private Directory root;
+    private static FileSystem fs = null;
+
+    private FileSystem() {
+        this.directory = new Directory(null, "root", "admin", 0);
+    }
+
+    public static FileSystem getFileSystem() {
+        if (fs == null) {
+            fs = new FileSystem();
+        }
+        return fs;
+    }
+
+    public List<FileSystemElement> getChildren(Directory current){
+        return current.getChildren();
+    }
+
+    public List<FileSystemElement> sort(Directory dir, Comparator<FileSystemElement> c){
+        List<FileSystemElement> ar = this.getChildren(dir);
+        Collections.sort(ar,c);
+        return ar;
+    }
+}
