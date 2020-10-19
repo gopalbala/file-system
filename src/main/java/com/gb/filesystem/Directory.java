9cd9fdf318f060f63e2be549a45f58c6779eac1c
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-10-19 23:41:36 +0530

    updated Directory

--- src/main/java/com/gb/filesystem/Directory.java
+++ src/main/java/com/gb/filesystem/Directory.java
@@ -3,8 +3,16 @@ package com.gb.filesystem;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 public class Directory extends FileSystemElement{
     private Directory parent;
+    private List<FileSystemElement> children = new ArrayList<>();
+    public Directory(Directory parent,String name,String owner,int size){
+        super(parent,name,owner,size);
+    }
+
 }
