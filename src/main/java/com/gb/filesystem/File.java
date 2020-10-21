7b71a2eced2de7b1a09eb0f4f84605af2fb6b825
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-10-22 00:03:37 +0530

    Added file

---
+++ src/main/java/com/gb/filesystem/File.java
@@ -0,0 +1,13 @@
+package com.gb.filesystem;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class File extends FileSystemElement {
+
+    public File(Directory parent, String name, String owner, int size) {
+        super(parent, name, owner, size);
+    }
+}
